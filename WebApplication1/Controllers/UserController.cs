6b0e7fa250c6dc8f13f6e407e2afcad27e76ddb2
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2026-03-03 11:28:19 +0100

    profilova stranka

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -17,6 +17,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult Profil()
+        {
+            return View();
+        }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
