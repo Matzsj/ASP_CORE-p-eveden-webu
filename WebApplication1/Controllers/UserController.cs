306f0fc70bd382d5cbb80f1eeb42cf5829db628d
Matyáš Jaworek <jaworekm24@spstrutnov.cz>
2026-03-03 10:55:58 +0100

    registrace a přihlášení

---
+++ WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
